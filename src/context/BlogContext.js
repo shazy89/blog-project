4afd2b45d5246a853e8864f4eb239d3de0775cb4
shazy89 <erdo.shazy123@gmail.com>
2020-12-23 17:10:44 -0500

    users can create Blog posts from create screen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,7 +5,11 @@ const blogReducer = (state, action) => {
     case 'add_blogpost':
       return [...state,
                { id: Math.floor(Math.random() * 99999), 
-                             title: `Blog Post #${state.length + 1}` } ];
+                 title: action.payload.title,
+                 content: action.payload.content 
+                } 
+              ];
+       
     case 'remove_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload)                   
     default:
@@ -14,8 +18,8 @@ const blogReducer = (state, action) => {
 };
      
 const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (title, content) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
   };
 };
 
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,17 +1,21 @@
 import React, {useState, useContext} from 'react';
-import { Text, StyleSheet, View, TextInput } from "react-native";
-
+import { Text, StyleSheet, View, TextInput, Button } from "react-native";
+import { Context } from '../context/BlogContext'
 
 const CreateScreen = ({navigation}) => {
   const [title, settitle] = useState('');
   const [content, setContent] = useState('');
 //console.log(title)
+  const { addBlogPost } = useContext(Context)
    return (
        <View> 
-            <Text> Enter Title:</Text>
-            <TextInput value={title} onChangeText={(text) => settitle(text)}/>
-            <Text> Create Contetnt:</Text>
-            <TextInput value={content} onChangeText={(text) => setContent(text)}/>
+            <Text style={styles.label}> Enter Title:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={(text) => settitle(text)}/>
+            <Text style={styles.label}> Create Contetnt:</Text>
+            <TextInput style={styles.input} value={content} onChangeText={(text) => setContent(text)}/>
+            <Button 
+               title='Add Blog Post'
+               onPress={() => addBlogPost(title, content)}  />
        </View>
    );
 };
@@ -19,7 +23,19 @@ const CreateScreen = ({navigation}) => {
       
 
 const styles = StyleSheet.create({
-
-});
-
-export default CreateScreen;
+   input: {
+     fontSize: 18,
+     borderWidth: 1,
+     borderColor: 'black',
+     marginBottom: 15,
+     padding: 5,
+     margin: 5,
+     borderRadius: 10
+    },
+    label: {
+     fontSize: 18,
+     marginBottom: 5,
+    }
+ });
+ 
+ export default CreateScreen;
