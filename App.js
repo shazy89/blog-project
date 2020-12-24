48553546a6a1d164020229a63b4e163d4e7f2c44
shazy89 <erdo.shazy123@gmail.com>
2020-12-24 11:52:58 -0500

    clean up code

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext'
+//import { Provider as BlogComments } from './src/context/BlogComments' for another provider
 import ShowScreen from './src/screens/ShowScreen'
 import CreateScreen from './src/screens/CreateScreen'
 import EditScreen from './src/screens/EditScreen'
@@ -28,3 +29,13 @@ export default () => {
            <App />
          </Provider>  
 };
+
+//const App = createAppContainer(navigator)
+// to add another provider
+// export default () => {
+//   return <Provider>
+//            <CommentsProvider>
+//             <App />
+//           </CommentsProvider>
+//          </Provider>  
+// };
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,5 +1,6 @@
 import createDataContext from './createDataContext';
 import jsonServer from "../api/jsonServer";
+
 //any time we have to add some kid a new resource just def reducer def functions retur dispcatch and createContext..
 const blogReducer = (state, action) => { 
   switch (action.type) {
@@ -17,35 +18,36 @@ const blogReducer = (state, action) => {
 };
 
 
-const getBlogPosts = dispatch => {
-  return async () => {
-   const response = await jsonServer.get('/blogposts');
-    dispatch({ type: 'get_blogposts', payload: response.data})
-  };
-};
-
-const addBlogPost = dispatch => {
-  return async (title, content, callback) => {
-    await jsonServer.post('/blogposts', { title, content });
-    if(callback) callback();
-  };
-};
-
-  
-const editBlogPost = dispatch => {
-  return async (id, title, content,  callback) => {
-    await jsonServer.put(`/blogposts/${id}`, {title, content})
-     dispatch({type: 'edit_blogpost', payload: {id, title, content }});
+ const getBlogPosts = dispatch => {
+   return async () => {
+    const response = await jsonServer.get('/blogposts');
+     dispatch({ type: 'get_blogposts', payload: response.data})
+   };
+ };
+ 
+ const addBlogPost = dispatch => {
+   return async (title, content, callback) => {
+     await jsonServer.post('/blogposts', { title, content });
      if(callback) callback();
-  };
-};
+   };
+ };
+ 
+   
+ const editBlogPost = dispatch => {
+   return async (id, title, content,  callback) => {
+     await jsonServer.put(`/blogposts/${id}`, {title, content})
+      dispatch({type: 'edit_blogpost', payload: {id, title, content }});
+      if(callback) callback();
+   };
+ };
+ 
+ const removeBlogPost = dispatch => {
+   return async (id) => { 
+     await jsonServer.delete(`/blogposts/${id}`)
+     dispatch({type: 'remove_blogpost', payload: id})
+   };
+ };
 
-const removeBlogPost = dispatch => {
-  return async (id) => { 
-    await jsonServer.delete(`/blogposts/${id}`)
-    dispatch({type: 'remove_blogpost', payload: id})
-  };
-};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
